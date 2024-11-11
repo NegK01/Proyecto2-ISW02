493b2b01290bd524d57bc747d84098f68a910433
Neg <mauricio.or99@gmail.com>
2024-11-11 13:25:45 -0600

    Refinamineto de codigo actual

--- src/Capa_Logica/Main.java
+++ src/Capa_Logica/Main.java
@@ -6,7 +6,6 @@ package Capa_Logica;
 
 import Capa_Interfaz.Frm_Login;
 import com.formdev.flatlaf.themes.*;
-import java.net.URISyntaxException;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -25,7 +24,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws URISyntaxException {
+    public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(new FlatMacLightLaf());
         } catch (UnsupportedLookAndFeelException ex) {
