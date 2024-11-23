92808c6ddd4fe580cb6a52e5366726e1fe6cd8e1
Neg <92654015+NegK01@users.noreply.github.com>
2024-11-23 12:52:26 -0600

    Update SQLProyectoII.sql

--- SQLProyectoII.sql
+++ SQLProyectoII.sql
@@ -9,156 +9,175 @@ Use TransportesIIIPatitos
 ---------------------------------------------------------|
 -- Crear Tablas -----------------------------------------|
 
--- *Usuarios* -- 
+-- //Usuarios\\ -- 
 CREATE TABLE Usuarios(   
     id	        int primary key,
 	usuario		varchar(55) unique not null,
 	contrasena	varchar(256) not null,
 	rol			int not null, -- 1 = Admin  / 0	 = Usuario
-	activo      int not null, -- 1 = Activo / 0 = Inactivo
+	activo      int not null  -- 1 = Activo / 0 = Inactivo
 );
 
--- *vehiculos* --
+-- //empleados\\ --
+CREATE TABLE empleados(
+	id	        int primary key,
+	cedula      int unique not null,
+	nombre		varchar(55) not null,
+	rol			int not null, -- 1 = Chofer / 0 = Mecanico
+	activo		int not null
+	CONSTRAINT fk_idEmpleado	FOREIGN KEY(id) REFERENCES Usuarios(id)
+);
+
+-- //vehiculos\\ 
 CREATE TABLE vehiculos(
     id					int PRIMARY KEY,
     placa				varchar(20) UNIQUE NOT NULL,
-	nombre              varchar(65) not null,
-	marca				varchar(65) not null,
-    modelo				varchar(55) not null,
+	marca              varchar(65) not null,
+	modelo				varchar(65) not null,
+    anio				varchar(55) not null,
     id_combustible  	int NOT NULL, 
     activo				int NOT NULL -- activo = 1, inactivo = 0
 	CONSTRAINT fk_combustible	FOREIGN KEY(id_combustible) REFERENCES combustibles(id)
 );
 
--- *combustibles* --
+-- //combustibles\\ --
 CREATE TABLE combustibles(
     id				   int PRIMARY KEY,
     nombre             varchar(55) UNique NOT NULL,
     activo             int NOT NULL -- activo = 1, inactivo = 0
 );
 
-Select * from tanques_combustible
-
--- ///tanques_combustible\\\ -- 
+-- //tanques_combustible\\ -- 
 CREATE TABLE tanques_combustible(
     id							int PRIMARY KEY,
 	nombre						varchar(60) Unique NOT NULL,
 	capacidad					decimal NOT NULL,
     id_combustible				int NOT NULL,
-	activo                      int NOT NULL,
-    CONSTRAINT foreing_combustible	FOREIGN KEY(id_combustible) REFERENCES combustibles(id) -- se cambio la foreing porque se repetia el nombre
+	activo                      int NOT NULL
+    CONSTRAINT foreing_combustible	FOREIGN KEY(id_combustible) REFERENCES combustibles(id)
 );
 
--- ingreso_de_combustible (factura) -- ...................
-CREATE TABLE ingreso_de_combustible(
-    id							int PRIMARY KEY,
-    id_dispensado				int NOT NULL,
-    fecha_ingreso				date NOT NULL,
-    cantidad					decimal(10,2) NOT NULL,
-    CONSTRAINT fk_dispensado	FOREIGN KEY(id_dispensado) REFERENCES dispensadores(id)
-);
-
--- proveedores -- 
+-- //proveedores\\ -- 
 CREATE TABLE proveedores(
     id								int PRIMARY KEY,
     nombre							varchar(100) NOT NULL,
     direccion						varchar(255),
-    telefono						varchar(15),
     email							varchar(100),
-    --fecha_compra					date NOT NULL,
-    --id_usuario						int NOT NULL,
-    --CONSTRAINT fk_usuario_compra	FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+    activo							int NOT NULL
 );
 
--- *compra_combustible* ---
-CREATE TABLE compra_combustible(
-	id								int primary key,
-	fecha							date not null,
-	hora							varchar(10) not null,
-	id_proveedor					int not null,
-	activo							int not null
-	CONSTRAINT fk_proveedor	FOREIGN KEY(id_proveedor) REFERENCES proveedores(id)
+-- //ingreso_de_combustible Encabezado\\ -- (compra combustible)
+CREATE TABLE ingreso_de_combustible(
+    id							int PRIMARY KEY,
+    id_proveedor				int NOT NULL,
+    fecha_ingreso				date NOT NULL,
+	activo                      int NOT NULL
+    CONSTRAINT fk_combustibleProveedor	FOREIGN KEY(id_proveedor) REFERENCES proveedores(id)
 );
 
--- *dispensadores* --
+-- //ingreso_de_combustible Detalle\\ --
+CREATE TABLE ingreso_de_combustible_detalle(
+    id							int PRIMARY KEY,
+	id_ingreso_encabezado		int not null,
+	id_combustible				int not null,
+	id_tanque					int not null,
+    cantidad_comprada			decimal not null, -- cantidad de combustible comprado
+    costo_de_compra				decimal not null  -- costo de la compra de ese combustible
+    CONSTRAINT fk_ingreso_encabezado	FOREIGN KEY(id_ingreso_encabezado) REFERENCES ingreso_de_combustible(id),
+	CONSTRAINT fk_ingreso_combustible	FOREIGN KEY(id_combustible) REFERENCES combustibles(id),
+	CONSTRAINT fk_ingreso_tanque		FOREIGN KEY(id_tanque) REFERENCES tanques_combustible(id)
+);
+
+-- //dispensadores\\ --
 CREATE TABLE dispensadores(
-    id						int PRIMARY KEY,
-	nombre					varchar(60) Unique NOT NULL,
-	descripcion				varchar(255),
-    id_tanque				int NOT NULL,
-	activo					int NOT NULL,
-    CONSTRAINT fk_tanque	FOREIGN KEY(id_tanque) REFERENCES tanques_combustible(id)-- cantidad
+    id							int PRIMARY KEY,
+	nombre						varchar(60) Unique NOT NULL,
+	descripcion					varchar(255) NOT NULL, -- una descipcion por si desea agregar mas detalles como su ubicacion
+    id_tanque					int NOT NULL,
+	id_combustible				int NOT NULL,
+	activo						int NOT NULL
+    CONSTRAINT fk_tanque				FOREIGN KEY(id_tanque) REFERENCES tanques_combustible(id),-- cantidad
+	CONSTRAINT fk_dispensar_combustible	FOREIGN KEY(id_combustible) REFERENCES combustibles(id)
 );
 
--- dispensando_de_combustible -- .........................
+-- //dispensando_de_combustible\\ -- 
 CREATE TABLE dispensando_de_combustible(
     id								int PRIMARY KEY,
-    fecha_dispension				date NOT NULL,
-    id_dispensador					int NOT NULL,
+	id_dispensador					int NOT NULL,
     id_vehiculo						int NOT NULL,
-    id_usuario						int NOT NULL,
-    cantidad_dispensa				decimal(10,2) NOT NULL,
-    CONSTRAINT fk_dispensador		FOREIGN KEY(id_dispensador) REFERENCES dispensadores(id),
-    CONSTRAINT fk_vehiculo			FOREIGN KEY(id_vehiculo) REFERENCES vehiculos(id),
-    CONSTRAINT fk_usuario_dispensa	FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+    id_chofer						int NOT NULL,
+	id_combustible					int NOT NULL,
+	kilometraje						int NOT NULL,
+    fecha_dispension				date NOT NULL,
+    activo							int NOT NULL
+    CONSTRAINT fk_dispensador				FOREIGN KEY(id_dispensador) REFERENCES dispensadores(id),
+    CONSTRAINT fk_vehiculo					FOREIGN KEY(id_vehiculo) REFERENCES vehiculos(id),
+    CONSTRAINT fk_chofer_dispensado			FOREIGN KEY(id_chofer) REFERENCES usuarios(id),
+	CONSTRAINT fk_combustible_dispensado	FOREIGN KEY(id_combustible) REFERENCES combustibles(id)
 );
 
--- *partes_de_equipos (partes reemplazables)* --
+-- //partes_de_equipos\\ --
 CREATE TABLE partes_de_equipos(
     id		           int PRIMARY KEY,
-	--id_vehiculo		   int NOT NULL,
-    --nombre             varchar(100) NOT NULL,
     descripcion        varchar(255) NOT NULL,
-    --precio             decimal(10,2) NOT NULL,
-    --cantidad_stock     int NOT NULL,
 	activo             int NOT NULL
 );
 
--- *tipos_de_mantenimientos* --
+-- //tipos_de_mantenimientos\\ --
 CREATE TABLE tipos_de_mantenimientos(
     id				   int PRIMARY KEY,
-    descripcion        varchar(255) NOT NULL,
-    --precio             decimal(10,2) NOT NULL,	
+    descripcion        varchar(255) NOT NULL,	
 	activo             int NOT NULL
 );
 
--- **asignacion_de_mantenimientos** --
+-- //asignacion_de_mantenimientos\\ --
 CREATE TABLE asignacion_de_mantenimientos(
     id										int PRIMARY KEY,
-    id_placa_vehiculo						varchar(20) NOT NULL,
-    id_mantenimiento						int NOT NULL,
-	id_parte_de_equipo						int NOT NULL,
-    dias									int,
-    kilometraje								int,
-	activo                                  int NOT NULL,
-    CONSTRAINT fk_vehiculo_mantenimiento	FOREIGN KEY(id_placa_vehiculo) REFERENCES vehiculos(placa),
-    CONSTRAINT fk_mantenimiento				FOREIGN KEY(id_mantenimiento) REFERENCES tipos_de_mantenimientos(id),
-	CONSTRAINT fk_parte_de_equipo		    FOREIGN KEY(id_parte_de_equipo) REFERENCES partes_de_equipos(id)
+    id_vehiculo								varchar(20) NOT NULL,
+	id_parte_del_equipo						int NOT NULL,
+    id_tipo_mantenimiento					int NOT NULL,	
+	kilometraje								int, -- kilometraje de duracion del mantenimiento
+    dias									int, -- dias de duracion del mantenimiento
+	activo                                  int NOT NULL
+    CONSTRAINT fk_vehiculo_mantenimiento	FOREIGN KEY(id_vehiculo) REFERENCES vehiculos(placa),
+	CONSTRAINT fk_parte_de_equipo		    FOREIGN KEY(id_parte_del_equipo) REFERENCES partes_de_equipos(id),
+	CONSTRAINT fk_mantenimiento				FOREIGN KEY(id_tipo_mantenimiento) REFERENCES tipos_de_mantenimientos(id)
 );
+/*
+	-en el primer dispensado de combustible el auto tiene 1000km.
+	-hace un mantenimiento que tiene de kilometraje 7000km de vida util.
+	-si en el siguiente registro de dispensado tiene 7000km aun le quedan 1000km para usar.
+	-si en el siguiente registro de dispensado tiene 7500km se debe advertir que esta cerca 
+		de la caducidad y que se debe hacer un mantenimiento.
+	-si en el siguiente registro de dispensado tiene 8000km se debe mostrar de manera resaltada
+		que acaba de caducar y se debe hacer un mantenimiento por estar sobrefecha, obviamente
+		se debe indicar de que es el cambio, si es de llantas o de cambio de aceite u otra cosa.
+*/
 
--- *boletas_de_mantenimientos_encabezado* --
+-- //boletas_de_mantenimientos_encabezado\\ --
 CREATE TABLE boletas_de_mantenimientos_encabezado(
     id								int PRIMARY KEY,
-	id_placa_vehiculo				varchar(20) NOT NULL,
-	mecanico						varchar(50) NOT NULL,
-	chofer							varchar(50) NOT NULL,
-    kilometraje						int NOT NULL,
-	fecha                           date NOT NULL, -- puede ser fecha
-    CONSTRAINT fk_placa_vehiculo	FOREIGN KEY(id_placa_vehiculo) REFERENCES vehiculos(placa)
+	id_vehiculo						varchar(20) NOT NULL,
+    kilometraje						int NOT NULL, -- kilometraje actual de vehículo
+	mecanico						int NOT NULL,
+	fecha_emision					date NOT NULL, -- fecha de emision de la boleta
+	activo							int NOT NULL
+    CONSTRAINT fk_placa_vehiculo			FOREIGN KEY(id_vehiculo) REFERENCES vehiculos(placa),
+	CONSTRAINT fk_mecanico_mantenimiento	FOREIGN KEY(mecanico) REFERENCES empleados(id)
 );
 
--- *boletas_de_mantenimientos_detalle* --
+-- //boletas_de_mantenimientos_detalle\\ --
 CREATE TABLE boletas_de_mantenimientos_detalle(
     id								int NOT NULL,
-	id_mantenimiento				int NOT NULL,
+	id_tipo_mantenimiento			int NOT NULL,
 	id_parte_de_equipo				int NOT NULL,
-	id_boleta_encabezado			int NOT NULL,
-	CONSTRAINT fk_mantenimiento				FOREIGN KEY(id_mantenimiento) REFERENCES tipos_de_mantenimientos(id),
-	CONSTRAINT fk_parte_de_equipo		    FOREIGN KEY(id_parte_de_equipo) REFERENCES partes_de_equipos(id),
+	id_boleta_encabezado			int NOT NULL
+	CONSTRAINT fk_mantenimiento				FOREIGN KEY(id_tipo_mantenimiento) REFERENCES tipos_de_mantenimientos(id),
+	CONSTRAINT fk_parte_de_equipo		    FOREIGN KEY(id_parte_de_equipo)	REFERENCES partes_de_equipos(id),
 	CONSTRAINT fk_boleta_encabezado			FOREIGN KEY(id_boleta_encabezado) REFERENCES boletas_de_mantenimientos_encabezado(id)
 );
 
--- *bitacora* --
+-- //bitacora\\ --
 CREATE TABLE bitacora(
 	id								int primary key,
 	fecha							date not null,
@@ -169,46 +188,6 @@ CREATE TABLE bitacora(
 	CONSTRAINT fk_usuario	FOREIGN KEY(usuario) REFERENCES Usuarios(usuario)
 );
 
-
-
-
-CREATE TABLE ingreso_de_combustible(
-    id								int PRIMARY KEY,
-    id_dispensado					int NOT NULL,
-    fecha_ingreso					date NOT NULL,
-    cantidad						decimal(10,2) NOT NULL,
-    CONSTRAINT fk_dispensado	FOREIGN KEY(id_dispensado) REFERENCES dispensadores(id)
-);
-CREATE TABLE dispensando_de_combustible(
-    id								int PRIMARY KEY,
-    fecha_dispension				date NOT NULL,
-    id_dispensador					int NOT NULL,
-    id_vehiculo						int NOT NULL,
-    id_usuario						int NOT NULL,
-    cantidad_dispensa				decimal(10,2) NOT NULL,
-    CONSTRAINT fk_dispensador		FOREIGN KEY(id_dispensador) REFERENCES dispensadores(id),
-    CONSTRAINT fk_vehiculo			FOREIGN KEY(id_vehiculo) REFERENCES vehiculos(id),
-    CONSTRAINT fk_usuario_dispensa	FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
-);
---equipo_combustible                -----
---id								-1/2-
---tipo 01 entrada 02 salida			-1/2-
---dispensador						-1/2-
---combustible..						-1/2-
---tanque..							- 1 -
---id_compra							- 1 -
---vehiculo							- 2 -
---km								- 2 -
---fecha-hora						-1/2-
---cantidad dispensada				- 2 -
---estado							-1/2-
-
---compra_combustible				-----
---id								-----
---fecba								-----
---hora								-----
---proveedir							-----
---estado							-----
 ---------------------------------------------------------|
 -- Pruebas ----------------------------------------------|
 DROP DATABASE TransportesIIIPatitos;
